06ab7c029fb7fa2312b101e760d386d47b0e2cf4
Khashayar Heydari <331684785+khashayar-heydari@users.noreply.github.com>
2026-09-20 21:00:58 +0200

    Parse and display departure data

--- src/entur_api.py
+++ src/entur_api.py
@@ -1,7 +1,7 @@
 import requests
 
-
-BASE_URL = "https://api.entur.io/geocoder/v3/autocomplete"
+GEOCODER_URL = "https://api.entur.io/geocoder/v3/autocomplete"
+JOURNEY_PLANNER_URL = "https://api.entur.io/journey-planner/v3/graphql"
 
 HEADERS = {
     "ET-Client-Name": "khashayar-norway-rail-data-explorer"
@@ -12,15 +12,59 @@ def search_station(station_name):
     params = {
         "q": station_name,
         "lang": "no",
-        "limit": 5,
+        "limit": 1,
         "layers": "stopPlace",
-        "stopPlaceTypes": "railStation",
+        "stopPlaceTypes": "railStation"
     }
 
     response = requests.get(
-        BASE_URL,
+        GEOCODER_URL,
         headers=HEADERS,
         params=params,
+        timeout=10
+    )
+
+    response.raise_for_status()
+
+    data = response.json()
+
+    if not data["features"]:
+        return None
+
+    station = data["features"][0]
+
+    return {
+        "name": station["properties"]["names"]["display"],
+        "id": station["properties"]["id"]
+    }
+
+
+def get_departures(stop_place_id):
+    query = """
+    {
+      stopPlace(id: "%s") {
+        estimatedCalls(numberOfDepartures: 5) {
+          aimedDepartureTime
+          expectedDepartureTime
+
+          destinationDisplay {
+            frontText
+          }
+
+          serviceJourney {
+            line {
+              publicCode
+            }
+          }
+        }
+      }
+    }
+    """ % stop_place_id
+
+    response = requests.post(
+        JOURNEY_PLANNER_URL,
+        headers=HEADERS,
+        json={"query": query},
         timeout=10
     )
 
@@ -29,12 +73,27 @@ def search_station(station_name):
     return response.json()
 
 
-data = search_station("Tønsberg")
+station = search_station("Tønsberg")
 
-for feature in data["features"]:
-    properties = feature["properties"]
+if station:
+    print("Station:", station["name"])
+    print("ID:", station["id"])
 
-    print("Name:", properties["names"]["display"])
-    print("ID:", properties["id"])
-    print("Coordinates:", feature["geometry"]["coordinates"])
-    print()
+    departures = get_departures(station["id"])
+
+    calls = departures["data"]["stopPlace"]["estimatedCalls"]
+
+    for call in calls:
+        line = call["serviceJourney"]["line"]["publicCode"]
+        destination = call["destinationDisplay"]["frontText"]
+
+        scheduled = call["aimedDepartureTime"][11:16]
+        expected = call["expectedDepartureTime"][11:16]
+
+        print()
+        print(f"{line} -> {destination}")
+        print(f"Scheduled: {scheduled}")
+        print(f"Expected:  {expected}")
+
+else:
+    print("Station not found")
